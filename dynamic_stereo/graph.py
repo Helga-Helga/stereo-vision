9a9c3121b8984904a5f1d0cacd0a2fbe788420b7
helga-helga <13537448+Helga-Helga@users.noreply.github.com>
2019-05-21 22:52:17 +0300

    Change code to follow pep8

--- dynamic_stereo/graph.py
+++ dynamic_stereo/graph.py
@@ -1,6 +1,6 @@
 from numpy import full, array
 from PIL import Image
-from Semiring.SemiringArgminPlusELement import SemiringArgminPlusElement
+from Semiring.SemiringArgminPlusElement import SemiringArgminPlusElement
 
 
 def initialize_graph(n_obj, max_disparity, semiring=SemiringArgminPlusElement):
@@ -12,12 +12,13 @@ def initialize_graph(n_obj, max_disparity, semiring=SemiringArgminPlusElement):
     n_obj * max_disparity -- number of nodes
 
     Weight of an edge is its length.
-    Edges go only between neighbour objects (from left to right)
+    Edges go only between neighbor objects (from left to right)
 
     :param semiring: defines generalized operations add and mul
     :return: 3d array of nodes and 3d array of edges.
     Example of indexed node and edge: (i, k, k'),
-    where i is object, k is a label in this object, k' is a label in the next object (i + 1)
+    where i is object, k is a label in this object,
+    k' is a label in the next object (i + 1)
     """
     nodes = full((n_obj, max_disparity, max_disparity), semiring.get_zero())
     edges = full((n_obj, max_disparity, max_disparity), semiring.get_zero())
@@ -28,7 +29,8 @@ def fill_nodes(nodes, left_image, right_image, line):
     """
 
     Label is a disparity of pixel (shift in pixels to left for left image).
-    Weight of a node is difference of intensities between left and right images.
+    Weight of a node is difference of intensities
+    between left and right images.
 
     nodes[obj, disparity, 0] is weight of node (obj, disparity)
     nodes[obj, disparity, 1] is a best label in next (obj + 1) object
@@ -45,7 +47,9 @@ def fill_nodes(nodes, left_image, right_image, line):
         for disparity in range(nodes.shape[1]):
             if obj - disparity >= 0:
                 print(left_image[obj, line] - right_image[obj-disparity, line])
-                nodes[obj, disparity, 0] = abs(left_image[obj, line] - right_image[obj-disparity, line])
+                nodes[obj, disparity, 0] =\
+                    abs(left_image[obj, line] -
+                        right_image[obj-disparity, line])
             nodes[obj, disparity, 1] = None
     return nodes
 
@@ -63,7 +67,8 @@ def fill_edges(edges):
             for disparity_next in range(edges.shape[2]):
                 if disparity_next > disparity + 1:
                     continue
-                edges[obj, disparity, disparity_next] = abs(disparity - disparity_next)
+                edges[obj, disparity, disparity_next] =\
+                    abs(disparity - disparity_next)
     return edges
 
 
